1dd56a842a9680f01527491c32d1d5b7008432ae
mutabor919 <119932972+mutabor919@users.noreply.github.com>
2025-05-11 22:31:56 +0300

    SmartPointer.h

--- lab10Main/SmartPointer.h
+++ lab10Main/SmartPointer.h
@@ -11,16 +11,16 @@ public:
         delete ptr;
     }
 
-    // Запрет копирования
+    
     SmartPointer(const SmartPointer&) = delete;
     SmartPointer& operator=(const SmartPointer&) = delete;
 
-    // Перемещение
+    
     SmartPointer(SmartPointer&& other) noexcept : ptr(other.ptr) {
         other.ptr = nullptr;
     } 
 
-    // Проверка на самоприсваивание
+    
     SmartPointer& operator=(SmartPointer&& other) {
         if (this != &other) {
             delete ptr;
@@ -30,15 +30,15 @@ public:
         return *this;
     }
 
-    // Операторы доступа
+    
     T& operator*() const { return *ptr; }
     T* operator->() const { return ptr; }
     T* get() const { return ptr; }
 
-    // Проверка на валидность
+    
     explicit operator bool() const { return ptr != nullptr; }
 
-    // Освобождение ресурса
+    
     void reset(T* p = nullptr) {
         delete ptr;
         ptr = p;
